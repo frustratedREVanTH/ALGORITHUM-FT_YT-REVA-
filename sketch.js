a62649e8acc31032d05bae887a7416da21dd234c
frustratedREVanTH <61193058+frustratedREVanTH@users.noreply.github.com>
2020-03-10 18:59:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,24 @@
 
-var fixedrect,movingrect
+var fixedRect, movingRect;
 
 function setup() {
-  createCanvas(800,400);
-  fixedrect=createSprite(400, 200, 50, 50);
-movingrect=createSprite(300,100,50,50);
-movingrect.shapeColor="green"
-fixedrect.shapeColor="green"
-movingrect.velocityY=-3;
-fixedrect.velocityY=-3
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
 
-
-
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background("yellow") 
- movingrect.x=World.mouseX;
- movingrect.y=World.mouseY;
- /*if(movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2
-  &&fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2
-  &&movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2
-  &&fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2){
-   movingrect.shapeColor="red";
-   fixedrect.shapeColor="red";
-  
-}
-else {movingrect.shapeColor="green"
-fixedrect.shapeColor="green"
-}*/
-  
-if(movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2
-  &&fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2){
-    fixedrect.velocityX=fixedrect.velocityX*(-1);
-    movingrect.velocityX=movingrect.velocityX*(-1);
-  }
+  background(0,0,0);  
 
-if( movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2
-  &&fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2){
-  fixedrect.velocityY=fixedrect.velocityY*(-1)
-  movingrect.velocityY=movingrect.velocityY*(-1)}
-drawSprites();
-
+ bounceoff(movingRect,fixedRect);
+  drawSprites();
 }
+
+
